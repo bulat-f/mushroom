bebf35af06b4b1366dd60fb7457ee2a3946e6c96
Fatbulat <fatbulat5@gmail.com>
2014-02-07 18:03:59 +0400

    Инициализация

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MushroomApp::Application.config.secret_key_base = '630ecb946d4b6d0b382b4b68ea1a540a63d8972b265a3af3fe7ad57017c2491754cceadbe898ee20d4a71a750d60b2350b435335089d97cba9aa45e9a4e2d92e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MushroomApp::Application.config.session_store :cookie_store, key: '_mushroom_app_session'
